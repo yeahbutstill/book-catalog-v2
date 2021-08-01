e439c2ad7e272fd91bc6f8522051de154513c9a7
Tedy Tri Saputro <detik19@gmail.com>
2021-08-01 19:10:52 +0700

    [ADD] add lombok DI

---
+++ src/main/java/com/subrutin/catalog/service/BookService.java
@@ -0,0 +1,9 @@
+package com.subrutin.catalog.service;
+
+import com.subrutin.catalog.dto.BookDetailDTO;
+
+public interface BookService {
+	
+	public BookDetailDTO findBookDetailById(Long bookId);
+
+}
