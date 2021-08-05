f04c5d0efad10c50f550a68ce72019659d18acff
Tedy Tri Saputro <detik19@gmail.com>
2021-08-05 09:40:56 +0700

    [ADD] lombok logging finish

--- src/main/java/com/subrutin/catalog/service/impl/BookServiceImpl.java
+++ src/main/java/com/subrutin/catalog/service/impl/BookServiceImpl.java
@@ -1,5 +1,7 @@
 package com.subrutin.catalog.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,11 @@ import com.subrutin.catalog.repository.BookRepository;
 import com.subrutin.catalog.service.BookService;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 
-
+@Slf4j
 @AllArgsConstructor
 @Service("bookService")
 public class BookServiceImpl implements BookService{
