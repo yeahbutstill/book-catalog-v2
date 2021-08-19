2b6e61db7484de2d2df63cd67597b75099ac6249
Tedy Tri Saputro <detik19@gmail.com>
2021-08-19 12:09:37 +0700

    [ADD] add api find ALL

--- src/main/java/com/subrutin/catalog/web/BookResource.java
+++ src/main/java/com/subrutin/catalog/web/BookResource.java
@@ -1,12 +1,19 @@
 package com.subrutin.catalog.web;
 
+import java.net.URI;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.subrutin.catalog.dto.BookCreateDTO;
 import com.subrutin.catalog.dto.BookDetailDTO;
 import com.subrutin.catalog.service.BookService;
 
@@ -20,6 +27,7 @@ public class BookResource {
 	
 	private final BookService bookService;
 
+	//nama yang salah /get-book/{bookId}
 	@GetMapping("/book/{bookId}")
 	public BookDetailDTO findBookDetail(@PathVariable("bookId") Long id) {
 		StopWatch stopWatch = new StopWatch();
@@ -30,4 +38,22 @@ public class BookResource {
 		return result;
 
 	}
+	
+	//nama yang salah /save-book /create-book
+	@PostMapping("/book")
+	public ResponseEntity<Void> createANewBook(@RequestBody BookCreateDTO dto){
+		bookService.createNewBook(dto);
+		return ResponseEntity.created(URI.create("/  ")).build();
+	}
+	
+	@GetMapping("/book")
+	public ResponseEntity<List<BookDetailDTO>> findBookList(){
+		return ResponseEntity.ok().body(bookService.findBookListDetail());
+		
+	}
+	
+	//PUT /book
+	
+	//DELETE /book
+	
 }
