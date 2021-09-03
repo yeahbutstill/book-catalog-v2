a63a39f897591ad87a5ee0db91426a3fbb690ceb
Tedy Tri Saputro <detik19@gmail.com>
2021-09-03 22:06:57 +0700

    [ADD] add insert entity using jparepository

---
+++ src/main/java/com/subrutin/catalog/dto/AuthorCreateRequestDTO.java
@@ -0,0 +1,20 @@
+package com.subrutin.catalog.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.Data;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Data
+public class AuthorCreateRequestDTO {
+
+	@NotBlank
+	private String authorName;
+	
+	@NotNull
+	private Long birthDate;
+}
