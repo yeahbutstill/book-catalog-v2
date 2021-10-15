e596e0d5806205b6278f858dde3453abfe2c01b7
Tedy Tri Saputro <detik19@gmail.com>
2021-10-15 17:52:15 +0700

    [ADD] add exception handler

--- src/main/java/com/subrutin/catalog/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/subrutin/catalog/exception/ExceptionHandlerAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,7 +17,16 @@ import com.subrutin.catalog.dto.ErrorResponseDTO;
 import com.subrutin.catalog.enums.ErrorCode;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class ExceptionHandlerAdvice extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	protected ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request){
+		List<String> details = new ArrayList<String>();
+		details.add(ex.getMessage());
+		ErrorResponseDTO errorResponse =  ErrorResponseDTO.of("data not found", details, ErrorCode.DATA_NOT_FOUND, HttpStatus.BAD_REQUEST);
+		return ResponseEntity.badRequest().body(errorResponse);
+
+	}
 
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
