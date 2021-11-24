e71c9f5e9f5790075b5f94a96e43868079de579d
Tedy Tri Saputro <detik19@gmail.com>
2021-11-25 06:49:41 +0700

    [ADD] add payload

--- src/main/java/com/subrutin/catalog/dto/AuthorUpdateRequestDTO.java
+++ src/main/java/com/subrutin/catalog/dto/AuthorUpdateRequestDTO.java
@@ -1,5 +1,7 @@
 package com.subrutin.catalog.dto;
 
+import java.util.List;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -12,4 +14,6 @@ public class AuthorUpdateRequestDTO {
 	private String authorName;
 
 	private Long birthDate;
+	
+	private List<AddressUpdateRequestDTO> addresses;
 }
