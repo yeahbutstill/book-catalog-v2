2bb5d9e71ba83b28bc48737f5dc7040fc5580e4a
Tedy Tri Saputro <detik19@gmail.com>
2022-02-11 08:18:34 +0700

    [add] add jwt

--- src/main/java/com/subrutin/catalog/config/AppConfig.java
+++ src/main/java/com/subrutin/catalog/config/AppConfig.java
@@ -1,14 +1,32 @@
 package com.subrutin.catalog.config;
 
+import java.security.Key;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.subrutin.catalog.security.util.JWTTokenFactory;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
 @Configuration
 public class AppConfig {
+	
+	@Bean
+	public Key key() {
+		byte[] keyBytes = Decoders.BASE64.decode("021kdasjkjsdaiwqueioewuq1292813021938123qwdsasdawqewq2121");
+		return Keys.hmacShaKeyFor(keyBytes);
+	}
+	
+	@Bean
+	public JWTTokenFactory jwtTokenFactory(Key key) {
+		return new JWTTokenFactory(key);
+	}
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
--- src/main/java/com/subrutin/catalog/security/handler/UsernamePasswordAuthSuccessHandler.java
+++ src/main/java/com/subrutin/catalog/security/handler/UsernamePasswordAuthSuccessHandler.java
@@ -7,14 +7,19 @@ import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.subrutin.catalog.security.model.AccessJWTToken;
+import com.subrutin.catalog.security.util.JWTTokenFactory;
 
 import lombok.AllArgsConstructor;
 
@@ -23,17 +28,31 @@ import lombok.AllArgsConstructor;
 public class UsernamePasswordAuthSuccessHandler implements AuthenticationSuccessHandler {
 	
 	private final ObjectMapper mapper;
+	
+	private final JWTTokenFactory tokenFactory;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
-
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+		AccessJWTToken token =  tokenFactory.createAccessJWTToken(userDetails.getUsername(), userDetails.getAuthorities());
 		Map<String, String> resultMap = new HashMap<>();
-		resultMap.put("result", "SUCCESS");
+		resultMap.put("token", token.getToken());
 		response.setStatus(HttpStatus.OK.value());
 		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
 		mapper.writeValue(response.getWriter(), resultMap);
+		clearAuthenticationAttributes(request);
 
+	}
+	
+	protected final void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		
+		if(session==null) {
+			return;
+		}
+		
+		session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
 	}
 
 }
